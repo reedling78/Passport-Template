4d12e6046daab990bc15957a584f45084c6879f8
Reed Rizzo <reedling78@gmail.com>
2015-08-11 14:01:21 -0500

    New Init

--- README.md
+++
@@ -1,2 +0,0 @@
-Passport Template
-============
---
+++ config/database.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Fer on 10/02/2015.
+ */
+
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://ferni:asdf@proximus.modulusmongo.net:27017/Xym9osuq' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
